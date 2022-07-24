17601ce01d46bbc412b17f858562aebc272192cf
synchroazel <antonio.padalino.me@gmail.com>
2022-07-24 16:24:36 +0200

    configured for Vercel

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from webapp import create_app
-from flask import Flask
-
-app = Flask(__name__)
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run()
+# from webapp import create_app
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# app = create_app()
+#
+# if __name__ == '__main__':
+#     app.run()
--- webapp/auth.py
+++ webapp/auth.py
@@ -23,7 +23,7 @@ def login():
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
-            flash('Email does not exist.', category='error')
+            flash('Username does not exist.', category='error')
 
     return render_template("login.html", user=current_user)
 
