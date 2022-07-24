cc9761a7afd2a2d825cc4ef9857edf56fb059186
synchroazel <antonio.padalino.me@gmail.com>
2022-07-24 14:34:31 +0200

    renamed views.py

---
+++ webapp/index.py
@@ -0,0 +1,31 @@
+from flask import Flask, Blueprint, render_template, request, flash
+from flask_login import login_required, current_user
+from eshop_handlers import get_game_info
+
+from webapp import db
+from .models import Wishlist
+
+app = Flask(__name__)
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        id_to_remove = request.form["remove_game"]
+
+        Wishlist.query.filter_by(content=id_to_remove, user_id=current_user.id).delete()
+        db.session.commit()
+        flash('Game successfully removed from your wishlist!', category='error')
+
+    user_wishlist = Wishlist.query.filter_by(user_id=current_user.id).all()
+
+    payload = list()
+
+    for game in user_wishlist:
+        info = get_game_info(game.content)
+        payload.append(info)
+
+    return render_template("home.html", user=current_user, payload=payload)
